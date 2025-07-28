19e08b5311fc5f53dab6f9d8301669c06a1e5b23
Miguel Ceballos <mceballosleonides@gmail.com>
2025-07-27 23:21:56 -0600

    Muestra una spinner mientras carga los videos y agrega vista con scroll vertical

--- lib/presentation/screens/discover/discover_screen.dart
+++ lib/presentation/screens/discover/discover_screen.dart
@@ -1,15 +1,23 @@
-
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:toktik/presentation/widgets/shared/video_scrollable_view.dart';
+
+import '../../providers/discover_provider.dart';
 
 class DiscoverScreen extends StatelessWidget {
   const DiscoverScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Discover Screen'),
-      ),
-    ); 
+    final discoverProvider = context
+        .watch<
+          DiscoverProvider
+        >(); // Agregamos el provider que deseamos escuchar en esta screen cuando se realiza un cambio
+
+    return Scaffold(
+      body: discoverProvider.isLoading
+          ? const Center(child: CircularProgressIndicator(strokeWidth: 2)) // Muestra un spinner mientras carga
+          : VideoScrollableView(videos: discoverProvider.videos), // Muestra los videos
+    );
   }
 }
