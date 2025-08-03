e7131a2b31ffda39bc368d28ce2dc7c099bbe672
Miguel Ceballos <mceballosleonides@gmail.com>
2025-08-03 14:41:25 -0600

    instala video_player, cambia fullscreen_player de statalesswidget a statefulwidget y agrega el controlador del video, agrega permisos a android

--- lib/presentation/widgets/shared/video_scrollable_view.dart
+++ lib/presentation/widgets/shared/video_scrollable_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:toktik/domain/entities/video_post.dart';
 import 'package:toktik/presentation/widgets/shared/video_buttons.dart';
+import 'package:toktik/presentation/widgets/video/fullscreen_player.dart';
 
 class VideoScrollableView extends StatelessWidget {
   final List<VideoPost> videos;
@@ -18,6 +19,12 @@ class VideoScrollableView extends StatelessWidget {
         return Stack(
           children: [
             //Video Player + Gradient
+            SizedBox.expand(
+              child: FullscreenPlayer(
+                caption: videoPost.caption,
+                videoUrl: videoPost.videoUrl,
+              )
+            ),
 
             // Buttons
             Positioned(
